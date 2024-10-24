1ed3a518a98c6d60c9d346e173a4f0c2684f2a6c
Rafael Costa <Rafael.Costa.T0127545@edu.atec.pt>
2024-10-24 22:40:30 +0100

    Update

--- obj/Debug/net8.0/Projeto.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a05cb54cae6a6dcf6cb7b75e3003684fb042844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77cdf8a66894f9d93429eb058c80dba07ee1b86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
